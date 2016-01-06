d0b7fdffd6dfb586ced821e2b1e8c44f845928cc
xeecos <flashindream@gmail.com>
2016-01-06 12:06:12 +0800

    fix timeout bug

--- makeblock.js
+++ makeblock.js
@@ -87,7 +87,7 @@
 				_isParseStart = false;
 
 				var position = _isParseStartIndex+2;
-				var extID = _buffer[position];
+				var extId = _buffer[position];
 				position+=1;
 				var type = _buffer[position];
 				position+=1;
@@ -113,7 +113,8 @@
 					value = readDouble(position);
 				}
 				if(type<=5){
-					_selectors["callback_"+extID](value);
+					_selectors["value_"+extId] = value;
+					_selectors["callback_"+extId](value);
 				}
 				_buffer = []
 			}
@@ -198,10 +199,27 @@
 	   	}, 20);
 	};
 	var lastWritten = 0;
-	function writePackage(buffer,callback){
-		if(new Date().getTime()-lastWritten>20){
-			lastWritten = new Date().getTime();
-			device.write(buffer,callback); 
+	var _buffers = [];
+	var _isWaiting = false;
+	function addPackage(buffer,callback){
+		_buffers.push(buffer);
+		var extId = buffer[4];
+		setTimeout(function(){
+			callback(_selectors["value_"+extId]);
+		},200);
+		writePackage();
+	}
+	function writePackage(){
+		if(_buffers.length>0&&_isWaiting==false){
+			_isWaiting = true;
+			var buffer = _buffers[0];
+			_buffers.shift();
+			device.write(buffer,function(){
+				setTimeout(function(){
+					_isWaiting = false;
+					writePackage();
+				},20);
+			}); 
 		}
 	}
 	ext._getStatus = function() {
@@ -241,7 +259,7 @@
 	}
     ext.resetAll = function(){
     	var data = [0x5,0xff, 0x55, 0x02, 0x0, 0x04];
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		})
     };
     ext.runBot = function(lSpeed,rSpeed){
@@ -249,7 +267,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId].concat(short2array(-lSpeed)).concat(short2array(rSpeed));
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
     }
     ext.runMotor = function(port,speed){
@@ -260,7 +278,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId, port].concat(short2array(speed));
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
     }
     ext.runServo = function(port,slot,angle){
@@ -274,7 +292,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId, port, slot, angle];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
     }
     ext.runLedOnBoard = function(index,red,green,blue){
@@ -300,7 +318,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId, port, slot, index, red*1, green*1, blue*1];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
     }
 	ext.runBuzzer = function(tone,beat){
@@ -314,7 +332,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId].concat(short2array(tone)).concat(short2array(beat));
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
 	};
 	ext.stopBuzzer = function(){
@@ -329,7 +347,7 @@
 		var brightness = 6;
 		var data = [extId, 0x02, deviceId, port,1,brightness,3].concat(short2array(x)).concat(short2array(7+y)).concat([msg.length].concat(string2array(msg)));
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
 	}
 	ext.showTime = function(port,hour,dot,min){
@@ -341,7 +359,7 @@
 		var brightness = 6;
 		var data = [extId, 0x02, deviceId, port,3,brightness,dot==":"?1:0].concat(short2array(hour)).concat(short2array(min));
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
 	}
 	ext.runSevseg = function(port,num){
@@ -352,7 +370,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId, port].concat(float2array(num));
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
 	}
 	ext.runLightSensor = function(port,status){
@@ -366,7 +384,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId, port,status];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
 	}
 	ext.runShutter = function(port,shutter){
@@ -380,7 +398,7 @@
 		var extId = 0;
 		var data = [extId, 0x02, deviceId, port,shutter];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
+ 		addPackage(arrayBufferFromArray(data), function(){
  		});
 	}
 	ext.getButtonOnBoard = function(status,callback){
@@ -392,12 +410,10 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 			console.log("written");
- 		});
 		_selectors["callback_"+extId] = function(v){
 			callback(status==1?v>500:v<500);
 		}
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	var _lastTime = 0;
 	var _lastButtonStatus = [false,false];
@@ -412,13 +428,11 @@
 			var extId = genNextID(port,status);
 			var data = [extId, 0x01, deviceId, port];
 			data = [data.length+3, 0xff, 0x55, data.length].concat(data);
-	 		writePackage(arrayBufferFromArray(data), function(){
-	 			console.log("written");
-	 		});
 			_selectors["callback_"+extId] = function(v){
 				_lastButtonStatus[0] = status==1?v>500:v<500;
 				_lastButtonStatus[1] = !_lastButtonStatus[status];
 			}
+ 			addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
     	}
     	return _lastButtonStatus[status];
 		
@@ -431,10 +445,8 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 			console.log("written");
- 		});
 		_selectors["callback_"+extId] = callback; 
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getUltrasonic = function(port,callback){
 		if(typeof port=="string"){
@@ -444,11 +456,10 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = function(v){
 			callback(Math.floor(v*100.0)/100.0); 
 		}
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getLinefollower = function(port,callback){
 		if(typeof port=="string"){
@@ -458,9 +469,8 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getJoystick = function(port,ax,callback){
 		if(typeof port=="string"){
@@ -473,9 +483,8 @@
 		var extId = genNextID(port,ax);
 		var data = [extId, 0x01, deviceId, port, ax];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getPotentiometer = function(port,callback){
 		if(typeof port=="string"){
@@ -485,9 +494,8 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getSoundSensor = function(port,callback){
 		if(typeof port=="string"){
@@ -497,9 +505,8 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getLimitswitch = function(port,slot,callback){
 		if(typeof port=="string"){
@@ -512,9 +519,8 @@
 		var extId = genNextID(port,slot);
 		var data = [extId, 0x01, deviceId, port, slot];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getTemperature = function(port,callback){
 		if(typeof port=="string"){
@@ -524,11 +530,10 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = function(v){
 			callback(Math.floor(v*100)/100);
 		}
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getPirmotion = function(port,callback){
 		if(typeof port=="string"){
@@ -538,9 +543,8 @@
 		var extId = genNextID(port,0);
 		var data = [extId, 0x01, deviceId, port];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	ext.getIrRemote = function(code,callback){
 		var deviceId = 14;
@@ -557,9 +561,8 @@
 		var extId = genNextID(port,slot);
 		var data = [extId, 0x01, deviceId, 0, code];
 		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		writePackage(arrayBufferFromArray(data), function(){
- 		});
 		_selectors["callback_"+extId] = callback;
+ 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
 	}
 	var descriptor = {
         blocks: [
@@ -568,7 +571,6 @@
 			[" ", "set servo %d.port %d.slot angle %d.servovalue","runServo", "Port1","Slot1", 90],
 			[" ", "set led %d.lport %d.slot %d.index red%d.value green%d.value blue%d.value","runLed","on board","Slot1","all",0,0,0],
 			[" ", "play tone on note %d.note beat %d.beats","runBuzzer", "C4", "Half"],
-			[" ", "stop tone","stopBuzzer"],
 			[" ", "show face %d.port x:%n y:%n characters:%s","showCharacters", "Port1", 0,0,"Hello"],
 			[" ", "show time %d.port hour:%n %m.points min:%n","showTime", "Port1", 10,":",20],
 			[" ", "show drawing %d.port x:%n y:%n draw:%m.drawFace","showDraw", "Port1", 0,0,"        "],
@@ -577,9 +579,9 @@
 			[" ", "set light sensor %d.aport led as %d.switchStatus","runLightSensor", "Port3", "On"],
 			[" ", "set camera shutter %d.port as %d.shutter","runShutter","Port1", "Press"],
 			["-"],
-			["R", "light sensor %d.laport","getLightSensor","light sensor on board"],
 			["h", "when button %m.buttonStatus","whenButtonPressed","pressed"],
 			["R", "button %m.buttonStatus","getButtonOnBoard","pressed"],
+			["R", "light sensor %d.laport","getLightSensor","light sensor on board"],
 			["-"],
 			["R", "ultrasonic sensor %d.port distance","getUltrasonic","Port1"],
 			["R", "line follower %d.port","getLinefollower","Port1"],
