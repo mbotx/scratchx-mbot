0b65578f87475b0cb8b19398c0fecb4580b31cb8
xeecos <flashindream@gmail.com>
2016-01-07 17:15:33 +0800

    change id

--- makeblock-app.js
+++ makeblock-app.js
@@ -602,7 +602,7 @@
 			ircode:["A","B","C","D","E","F","↑","↓","←","→","Setting","R0","R1","R2","R3","R4","R5","R6","R7","R8","R9"],
 		}
     };
-    var makeblockAppID = "hciagmngckbmimcoebjajffobnaklbcg"; //unique app ID for Hummingbird Scratch App
+    var makeblockAppID = "clgdmbbhmdlbcgdffocenbbeclodbndh"; //unique app ID for Hummingbird Scratch App
     var mConnection;
     var mStatus = 0;
 
